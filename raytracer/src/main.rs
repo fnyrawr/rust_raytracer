4b58a4c0c9ecd937bd0393c6e7e267c31d3a4ad9
Florian Kate <s51541@bht-berlin.de>
2024-01-03 18:45:02 +0100

    array with const color prepared

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Test");
+    println!("generating test image");
+
+    // image dimensions
+    const WIDTH: usize = 400;
+    const HEIGHT: usize = 300;
+    const DATA_SIZE: usize = 3*WIDTH*HEIGHT;
+
+    // data array containing image's flattened pixel data (like [R G B R G B R G B ...])
+    let mut data: [f32; DATA_SIZE] = [0.0; DATA_SIZE];
+
+    // assign constant value
+    for i in 0..data.len() {
+        data[i] = 100.0;
+    }
 }
