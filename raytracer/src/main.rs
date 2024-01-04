e3b181ba66fd9ed729122e8f12416e7a59a83b28
Florian Kate <s51541@bht-berlin.de>
2024-01-04 22:15:42 +0100

    code optimizations (Vec<f64> instead of plain array to prevent stack overflow), doc comments added

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -1,5 +1,4 @@
-#[allow(dead_code)]
-
+/// Representation of a simple RGB Color
 #[derive(Debug, Clone, Copy)]
 struct Color {
     r: f64,
@@ -7,32 +6,39 @@ struct Color {
     b: f64,
 }
 
+#[allow(dead_code)]
 impl Color {
     pub fn new(r: f64, g: f64, b: f64) -> Color {
         Color{ r, g, b }
     }
 
-    fn add(a: Color, b: Color) -> Color {
-        let mut result = Color {
+    /// Add two colors returning a new color instance
+    /// ### Arguments
+    /// * `a`, `b`: Color references
+    fn add(a: &Color, b: &Color) -> Color {
+        Color {
             r: a.r + b.r,
             g: a.g + b.g,
             b: a.b + b.b,
-        };
-
-        result
+        }
     }
 
-    fn subtract(a: Color, b: Color) -> Color {
-        let mut result = Color {
+    /// Subtract color b from color a returning a new color instance
+    /// ### Arguments
+    /// * `a`, `b`: Color references
+    fn subtract(a: &Color, b: &Color) -> Color {
+        Color {
             r: a.r - b.r,
             g: a.g - b.g,
             b: a.b - b.b,
-        };
-
-        result
+        }
     }
 
-    fn multiply(s: f64, a: Color) -> Color {
+    /// multiply a color by a scalar returning a new color instance
+    /// ### Arguments
+    /// * `s`: scalar
+    /// * `a`: Color reference
+    fn multiply(s: f64, a: &Color) -> Color {
         Color {
             r: s * a.r,
             g: s * a.g,
@@ -40,7 +46,11 @@ impl Color {
         }
     }
 
-    fn divide(a: Color, s: f64) -> Color {
+    /// divide a color by a scalar returning a new color instance
+    /// ### Arguments
+    /// * `a`: Color reference
+    /// * `s`: scalar
+    fn divide(a: &Color, s: f64) -> Color {
         Color {
             r: a.r / s,
             g: a.g / s,
@@ -48,7 +58,10 @@ impl Color {
         }
     }
 
-    fn clamp(v: Color) -> Color {
+    /// clamp color values between 0 and 1 returning a new color instance
+    /// ### Arguments
+    /// * `v`: Color reference
+    fn clamp(v: &Color) -> Color {
         Color {
             r: v.r.min(1.0).max(0.0),
             g: v.g.min(1.0).max(0.0),
@@ -57,22 +70,31 @@ impl Color {
     }
 }
 
+/// Sampler method to get a Color between two given colors depending on position (diagonal gradient between 0,0 and max_x, max_y)
+/// ### Arguments
+/// * `x`, `y`: position on x and y axis
+/// * `width`, `height`: image dimensions
+/// * `color1`, `color2`: start and end Color of gradient
 fn get_gradient_color(x: usize, y: usize, width: usize, height: usize, color1: Color, color2: Color) -> Color {
     // assign diagonal gradient
     let w = (x as f64 + y as f64) / (width + height) as f64;
-    let color = Color::add(color1, Color::multiply(w, Color::subtract(color2, color1)));
-
-    color
+    Color::add(&color1, &Color::multiply(w, &Color::subtract(&color2, &color1)))
 }
 
-fn set_pixel(data: &mut [f64], x: usize, y: usize, width: usize, height: usize, color: Color) {
-    let i = 3 * (y * width + x);
-    data[i] = color.r;
-    data[i+1] = color.g;
-    data[i+2] = color.b;
+/// Push pixel to data vector (append pixel's flattened r, g, b values)
+/// ### Arguments
+/// * `data`: reference to Vec<f64> reference data array
+/// * `color`: pixel color to be appended
+fn push_pixel(data: &mut Vec<f64>, color: Color) {
+    data.push(color.r);
+    data.push(color.g);
+    data.push(color.b);
 }
 
-fn f64_to_u8(input: &[f64]) -> Vec<u8> {
+/// Convert f64 vector to u8 vector
+/// ### Arguments
+/// * `input`: Vec<f64> data array reference to be converted
+fn f64_to_u8(input: &Vec<f64>) -> Vec<u8> {
     let mut output = Vec::with_capacity(input.len());
 
     for src in input {
@@ -83,9 +105,14 @@ fn f64_to_u8(input: &[f64]) -> Vec<u8> {
     output
 }
 
-fn save_image_png(data: &[f64], width: usize, height: usize, path_str: &str) {
+/// Generate and save PNG-Image from Vec<f64> data array
+/// ### Arguments
+/// * `data`: Vec<f64> data array reference
+/// * `width`, `height`: dimensions of the image
+/// * `path_str`: relative path as String reference`
+fn save_image_png(data: &Vec<f64>, width: usize, height: usize, path_str: &str) {
     // convert float data to int
-    let scaled_data = f64_to_u8(&data);
+    let scaled_data = f64_to_u8(data);
 
     use std::io::BufWriter;
     use std::path::Path;
@@ -111,23 +138,24 @@ fn save_image_png(data: &[f64], width: usize, height: usize, path_str: &str) {
 
 fn main() {
     // image dimensions
-    const WIDTH: usize = 400;
-    const HEIGHT: usize = 200;
-    const DATA_SIZE: usize = 3*WIDTH*HEIGHT;
+    let width: usize = 1920;
+    let height: usize = 1080;
+    let data_size: usize = 3*width*height;
 
     // data array containing image's flattened pixel data (like [R G B R G B R G B ...])
-    let mut data: [f64; DATA_SIZE] = [0.0; DATA_SIZE];
+    //let mut data: [f64; DATA_SIZE] = [0.0; DATA_SIZE];
+    let mut data = Vec::<f64>::with_capacity(data_size);
 
     // assign color to data array (red to green gradient)
     let color1 = Color::new(255.0, 0.0, 0.0);
     let color2 = Color::new(0.0, 255.0, 0.0);
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            let color = get_gradient_color(x, y, WIDTH, HEIGHT, color1, color2);
-            set_pixel(&mut data, x, y, WIDTH, HEIGHT, color);
+    for y in 0..height {
+        for x in 0..width {
+            let color = get_gradient_color(x, y, width, height, color1, color2);
+            push_pixel(&mut data, color);
         }
     }
 
     // save image
-    save_image_png(&data, WIDTH, HEIGHT, "./doc/test_image.png");
+    save_image_png(&data, width, height, "./doc/test_image.png");
 }
